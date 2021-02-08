6ba65810ea8914f3096b21aeb2c6efef0328692f
cheng-shuai <cheng-shuai@hotmail.com>
2021-02-08 13:34:53 +0800

    首页文章列表-列表项-内容数据绑定

--- src/components/article-item/index.vue
+++ src/components/article-item/index.vue
@@ -1,5 +1,41 @@
 <template>
-  <div class="article-item-container">文章列表先</div>
+  <div class="article-item-container">
+    <van-cell>
+      <div class="article-title" slot="title">
+        {{article.title}}
+      </div>
+      <div class="article-describe" slot="label">
+        <div class="article-img-c" v-if="article.cover.type === 3">
+          <div>
+            <van-image
+            v-for="(img, index) in article.cover.images"
+            :key="index"
+            width="116"
+            height="73"
+            :src="img"
+          />
+          </div>
+        </div>
+        <div class="description">
+          <span>{{article.aut_name}}</span>
+          <span>{{article.comm_count}}评论</span>
+          <span>{{article.pubdate}}</span>
+        </div>
+      </div>
+      <div class="article-img-r"
+        v-if="article.cover.type === 1"
+      >
+        <div class="div" slot="default">
+          <van-image
+            width="116"
+            height="73"
+            fit="cover"
+            :src="article.cover.images[0]"
+          />
+        </div>
+      </div>
+    </van-cell>
+  </div>
 </template>
 
 <script>
