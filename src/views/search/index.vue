a907cca0343f646e523107c248cef789bd2056af
cheng-shuai <cheng-shuai@hotmail.com>
2021-02-10 09:55:38 +0800

    文章搜索-创建组件并配置路由

---
+++ src/views/search/index.vue
@@ -0,0 +1,30 @@
+<template>
+<div class='home-container'>搜索页面</div>
+</template>
+
+<script>
+
+export default {
+  name: 'SearchIndex',
+  components: {},
+  data () {
+    return {
+
+    }
+  },
+  computed: {},
+  watch: {},
+  methods: {
+
+  },
+  created () {
+
+  },
+  mounted () {
+
+  }
+}
+</script>
+<style lang=less scoped>
+
+</style>
